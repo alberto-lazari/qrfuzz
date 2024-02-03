6408bcfdee831f04ed6a3ae036452eddd60ef35b
Elia Scandaletti <elia.scandaletti@gmail.com>
2024-02-04 00:17:42 +0100

    Memoized dicts list

--- src/dictionary.ts
+++ src/dictionary.ts
@@ -3,17 +3,22 @@ import path from "path";
 
 const DICTS_DIR = path.resolve(__dirname, "dicts/");
 
-export const list_dicts = async () => {
-  const dir = await opendir(DICTS_DIR);
-  const ret: string[] = [];
-  if (dir !== undefined) {
+let memo: string[] | null = null;
+
+export const list_dicts = async (refresh = false): Promise<string[]> => {
+  if (refresh || memo === null) {
+    const list: string[] = [];
+
+    const dir = await opendir(DICTS_DIR);
     for await (const file of dir) {
       if (file.isFile()) {
         const fullname = file.name;
         const base = path.basename(fullname, path.extname(fullname));
-        ret.push(base);
+        list.push(base);
       }
     }
+
+    memo = list;
   }
-  return ret;
+  return memo;
 };
